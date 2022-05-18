274fda2699ecf4b8e2ad98e3ca5557fdec934c56
lobnasalah <89015244+lobnasalah@users.noreply.github.com>
2022-05-18 11:33:42 +0300

    Add files via upload

---
+++ revision.sql
@@ -0,0 +1,17 @@
+create database sales;
+use sales;
+CREATE TABLE IF NOT EXISTS test (
+    numbers INT(10),
+    words VARCHAR(10)
+);
+/*
+comment 
+comment
+*/
+#comment
+-- comment 
+SELECT 
+    *
+FROM
+    test
+DROP TABLE test;    
